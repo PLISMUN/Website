78862a95101ec7f1bb57d78d43ee78684e7a9b66
Nygosaki <63132234+Nygosaki@users.noreply.github.com>
2025-08-01 14:07:18 +0200

    cache fix 2

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,90 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-    eslint: {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+      source: '/',
+      headers: [
+        {
+        key: 'Cache-Control',
+        value: 's-maxage=1, stale-while-revalidate=59',
+        },
+      ],
+      },
+      {
+      source: '/faq',
+      headers: [
+        {
+        key: 'Cache-Control',
+        value: 's-maxage=1, stale-while-revalidate=59',
+        },
+      ],
+      },
+      {
+      source: '/about',
+      headers: [
+        {
+        key: 'Cache-Control',
+        value: 's-maxage=1, stale-while-revalidate=59',
+        },
+      ],
+      },
+      {
+      source: '/this-year',
+      headers: [
+        {
+        key: 'Cache-Control',
+        value: 's-maxage=1, stale-while-revalidate=59',
+        },
+      ],
+      },
+      {
+      source: '/legal/:path*',
+      headers: [
+        {
+        key: 'Cache-Control',
+        value: 's-maxage=1, stale-while-revalidate=59',
+        },
+      ],
+      },
+      {
+      source: '/user/:path*',
+      headers: [
+        {
+        key: 'Cache-Control',
+        value: 'no-store',
+        },
+      ],
+      },
+      {
+      source: '/api/:path*',
+      headers: [
+        {
+        key: 'Cache-Control',
+        value: 'no-store',
+        },
+      ],
+      },
+      {
+      source: '/:path*',
+      headers: [
+        {
+        key: 'Cache-Control',
+        value: 'no-store',
+        },
+      ],
+      },
+    ];
+  },
+  eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-    experimental: {
+  experimental: {
     viewTransition: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(req: Request) {
-  const response = NextResponse.next();
-  response.headers.set('Cache-Control', 'no-store');
-  return response;
-}
